8702ad634eb0c0dcfebed5c745f6b22f2bb80e28
Veronika Golovkova <veronika87979@gmail.com>
2025-11-17 00:09:17 +0500

    feat: 11/19

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -331,8 +331,24 @@ function isContainNumber(num, digit) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  let balanceIndex = -1;
+  let leftSum = 0;
+  let rightSum = 0;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    leftSum += arr[i];
+    rightSum = 0;
+    for (let j = i + 2; j < arr.length; j += 1) {
+      rightSum += arr[j];
+    }
+    if (leftSum === rightSum) {
+      balanceIndex = i + 1;
+      break;
+    }
+  }
+
+  return balanceIndex;
 }
 
 /**
